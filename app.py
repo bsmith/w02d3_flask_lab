772f451c4a60832b7b7028eacb5f0291f6802b6d
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-02 20:28:57 +0000

    Try the App Factory Pattern

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template
 from controllers.orders import orders
 
-app = Flask(__name__)
-app.register_blueprint(orders)
+# App Factory Pattern: https://flask.palletsprojects.com/en/2.2.x/patterns/appfactories/
+def create_app():
+    app = Flask(__name__)
+    app.register_blueprint(orders)
+    return app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app()
+    # app.run(debug=True)
+    app.run()
