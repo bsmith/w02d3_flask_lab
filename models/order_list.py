cd9debec8762f1b81aa473653b5e5ef4b3823be6
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-02 15:00:44 +0000

    Added lots and lots of styling

--- models/order_list.py
+++ models/order_list.py
@@ -8,3 +8,10 @@ order2 = Order("Colette",
     datetime.datetime(2022, 11, 2, 12, 12, 30),
      5, "hawaiian")
 orders = [order1, order2]
+
+
+for x in [1, 2, 3]:
+    orders.append(
+        Order("James", datetime.datetime(2022, 11, 2, 12, 12, 30),
+            10, "four cheese"))
+    
