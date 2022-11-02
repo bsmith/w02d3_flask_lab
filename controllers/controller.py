a21ccba3eec62b7c6568a985363e1403342f7df8
Benjamin Smith <bsmith@cabbage.org.uk>
2022-11-02 13:52:22 +0000

    Individual order pages

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route('/orders')
 def orders():
     return render_template('orders.html.j2', orders=order_list.orders)
+
+@app.route('/orders/<index>')
+def orders_view_one(index):
+    return render_template('order.html', order=order_list.orders[int(index)])
